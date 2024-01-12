7e95ccef3c178c397f4da03a872a0891e8b805ba
smease12 <scmease93@gmail.com>
2024-01-11 20:09:09 -0500

    Invalidate timer everytime different button is pressed in case button was pressed while another countdown was going down

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var counter = 0
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
         
         let hardness = sender.currentTitle!
         counter = eggTimes[hardness]!
         //print(eggTimes[hardness]!)
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
 
     }
     
@@ -28,7 +31,5 @@ class ViewController: UIViewController {
             print(counter)
             counter -= 1
         }
-        
     }
-    
 }
