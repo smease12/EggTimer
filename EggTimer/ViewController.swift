091361f5c37731ed5122d85860d0fa98c6070e60
smease12 <scmease93@gmail.com>
2024-01-11 19:13:51 -0500

    create dictionary for egg times, print value on button clicks

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,26 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
-        
-        if(hardness == "Soft")
-        {
-            print(softTime)
-        }
-        else if(hardness == "Medium")
-        {
-            print(mediumTime)
-        }
-        else if(hardness == "Hard")
-        {
-            print(hardTime)
-        }
+        let hardness = sender.currentTitle!
+        print(eggTimes[hardness]!)
         
     }
     
